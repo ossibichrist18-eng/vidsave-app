311d4e78cdac8a4fecaa36a8305b456b77dd6e61
OSSIBI Christ Meryl <ossibichrist18@gmail.com>
2026-06-22 22:05:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const youtubedl = require('youtube-dl-exec');
+const youtubedl = require('yt-dlp-exec');
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
